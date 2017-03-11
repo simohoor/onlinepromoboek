bbcb5f0d5b718c12429754c89d7f89714d433853
Simon Hoornaert <simon.hoornaert@student.arteveldehs.be>
2017-03-11 01:22:14 +0100

    upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//  $("document").ready(function(){
+//         $(".sort.btn").click(function(){
+//               $(".sort.btn").removeClass('active');
+//                $(this).toggleClass('active');
+//        });
+//  })
+
+      
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//       var button = document.querySelectorAll('.sort .btn');
+//       console.log(button.innerHTML);
+//       button.addEventListener("click", function(){
+//             button.classList.remove('active');
+//             button.classList.toggle('active');
+//   });
+// });
