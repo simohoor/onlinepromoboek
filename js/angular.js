27ba6187a342e79488d8e7dc6535676dde448370
Simon Hoornaert <simon.hoornaert@student.arteveldehs.be>
2017-03-20 18:51:58 +0100

    ff

--- js/angular.js
+++ js/angular.js
@@ -103,7 +103,7 @@
             $location.hash("student" + id);
             $location.path('/home');
             window.history.back();
- 
+            $anchorScroll();
         };
     });
 
@@ -147,7 +147,7 @@
                     data.studentnr = items[id - 1].studentnr;
 
                     defer.resolve(data);
-                }, 0);
+                }, 10);
                 return defer.promise;
             }
         };
